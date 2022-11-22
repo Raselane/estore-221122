0a08b867b405a052bb9393a819855560555ffd75
Refiloe <refiloeraselane@gmail.com>
2022-11-22 15:37:52 +0200

    Initial Commit

--- pages/api/products.ts
+++
@@ -1,9 +0,0 @@
-import {initMongoose} from '../../utils/mongoose';
-import Product from '../../models/Product';
-import db from '../../utils/db';
-
-export default async function handler(req:any, res:any){
-    await db.connect();
-
-    res.json(await Product.find().exec() );
-}
