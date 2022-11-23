14ae82a7ab1265a959bf8bc96c6162fe93fa7ddd
Refiloe <refiloeraselane@gmail.com>
2022-11-23 19:37:47 +0200

    Modified the Footer

--- components/Layout.js
+++ components/Layout.js
@@ -12,7 +12,7 @@ import DropdownLink from './DropdownLink';
 import shopping_cart from '../public/icons/shopping_cart.png';
 import dropdown from '../public/icons/dropdown.png';
 import Headerbottom from './Headerbottom';
-import Slider from "./Slider";
+import Slider from './Slider';
 
 export default function Layout({ title, children }) {
   const { status, data: session } = useSession();
@@ -162,14 +162,65 @@ export default function Layout({ title, children }) {
               )}
             </div>
           </nav>
-          <Headerbottom/>
-          {title === "Home Page" &&
-          <Slider/> 
-          }
-         </header>
+          <Headerbottom />
+          {title === 'Home Page' && <Slider />}
+        </header>
         <main className="container m-auto mt-4 px-4">{children}</main>
-        <footer className="flex h-10 justify-center items-center shadow-inner">
-          <p>Copyright © 2022 Amazona</p>
+        <footer className="bg-gray-100 text-center">
+          <div className="px-6 pt-6">
+            <form action="">
+              <div className="grid md:grid-cols-3 gird-cols-1 gap-4 flex justify-center items-center">
+                <div className="md:ml-auto md:mb-6">
+                  <p className="text-gray-800">
+                    <strong>Sign up for our newsletter</strong>
+                  </p>
+                </div>
+
+                <div className="md:mb-6">
+                  <input
+                    type="text"
+                    className="
+                              form-control
+                              block
+                              w-full
+                              px-3
+                              py-1.5
+                              text-base
+                              font-normal
+                              text-gray-700
+                              bg-white bg-clip-padding
+                              border border-solid border-gray-300
+                              rounded
+                              transition
+                              ease-in-out
+                              m-0
+                              focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+                            "
+                    id="exampleFormControlInput1"
+                    placeholder="Email address"
+                  />
+                </div>
+
+                <div className="md:mr-auto mb-6">
+                  <button
+                    type="button"
+                    className="inline-block px-6 py-2.5 bg-blue-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                  >
+                    Subscribe
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+
+          <div
+            className="text-center text-gray-700 p-4 bg-[rgba(0, 0, 0, 0.2)]"
+          >
+            © 2022 Copyright:
+            <a className="text-gray-800" href="https://estore-221122-1.vercel.app">
+              Digital Ecosystems Solutions
+            </a>
+          </div>
         </footer>
       </div>
     </>
